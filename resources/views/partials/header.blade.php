c08375222fc688d259f8d332bab81d4c44690b72
StefanDellamore <dellamoreste.info@gmail.com>
2024-02-23 17:52:16 +0100

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,55 @@
 <header>
-    header
+    <div class="my-container">
+        
+        <div class="header-top d-flex justify-content-end">
+            <p>dc power visa</p>
+            <p>ADDITIONAL DC SITES</p>
+        </div>
+
+        <div class="container">
+            <div class="header-button d-flex justify-content-between align-items-center">
+                <div class="logo">
+                    Logo
+                </div>
+                
+                <nav>
+                    <ul class="d-flex">
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">Tv</a>
+                        </li>
+                        <li>
+                            <a href="#">Collectibles</a>
+                        </li>
+                        <li>
+                            <a href="#">Videos</a>
+                        </li>
+                        <li>
+                            <a href="#">Fans</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop</a>
+                        </li>
+                        <div class="input-header">
+                            <input type="text" placeholder="search">
+                        </div>
+
+                    </ul>
+                    
+                </nav>
+                
+            </div>
+        </div>
+    </div>
 </header>
