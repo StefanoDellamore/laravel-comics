a1d3f7d374f014b9d653379c4c321ae3a051ef22
StefanDellamore <dellamoreste.info@gmail.com>
2024-02-23 18:58:05 +0100

    add main code

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,3 +1,37 @@
 <div>
-    prova push
+    {{-- Sfondo con immagine --}}
+    <div class="my-container">
+        wvwvwv
+    </div>
+
+    <div class="my-container">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-2">
+                    <div class="card-comic">
+                        <div>
+                            immagine
+                        </div>
+                        <p>title comics</p>
+                    </div>
+                </div>
+            </div>
+
+            <button>learn more</button>
+        </div>
+    </div>
+
+    <div class="my-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <div>
+                        immagine
+                    </div>
+                    <p>Digital Comics</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
