605512b2e07ef5bcad55e105010195393c70bd77
StefanDellamore <dellamoreste.info@gmail.com>
2024-02-25 14:09:06 +0100

    add style main-comics

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,24 +1,29 @@
 <div>
     {{-- Sfondo con immagine --}}
-    <div class="my-container">
-        wvwvwv
+    <div class="my-container jumbotron">
+        
     </div>
 
-    <div class="my-container">
+    <div class="my-container container-comics">
         <div class="container">
 
             <div class="row">
-                <div class="col-2">
+            @foreach ($comics as $comic)
+                <div class="col-2">                
                     <div class="card-comic">
-                        <div>
-                            immagine
-                        </div>
-                        <p>title comics</p>
+                       <div class="thumb-comic">
+                            <img src="{{ $comic['thumb'] }}" alt="">
+                       </div>
+
+                       <p>{{$comic['title']}}</p>
                     </div>
                 </div>
+            @endforeach
             </div>
-
-            <button>learn more</button>
+            <div class="d-flex justify-content-center">
+                <button class="button-comics">learn more</button>
+            </div>
+            
         </div>
     </div>
 
